58a367fbdd0b208795e092bf49048dee81d2001c
prasanth <prasanthshanmugam477@gmail.com>
2023-03-11 19:40:43 +0530

    test

--- features/environment.py
+++ features/environment.py
@@ -1,7 +1,1 @@
 import behave
-
-
-def before_all(context):
-    pass
-def after_all():
-    pass
--- features/steps/first.py
+++ features/steps/first.py
@@ -1,1 +1,19 @@
 import behave
+
+
+@given('we have behave installed')
+def step_impl(context):
+    print("given")
+    pass
+
+@when('we implement a test')
+def step_impl(context):
+    print("when")
+    assert True is not False
+
+@then('behave will test it for us!')
+def step_impl(context):
+    print(f"then")
+    assert context.failed is False
+
+
